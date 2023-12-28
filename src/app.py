e2bce338da34a4ef95c5bfe09187130bed8bef53
GennadyBr <gennady.budazhapov@gmail.com>
2023-12-28 12:13:05 +0300

    nginx 5.35.83.245/flask_mongo_app

--- src/app.py
+++ src/app.py
@@ -7,10 +7,11 @@ from settings import logger
 
 load_dotenv()
 
-app = Flask(__name__, root_path='/app_1')
+prefix = '/flask_mongo_app'  # nginx prefix name and container name
+app = Flask(__name__)
 
 
-@app.route("/app_1")
+@app.route(f"{prefix}")
 def show_user():
     """Вывод содержания базы данных на первой странице"""
     users = []
@@ -18,14 +19,14 @@ def show_user():
     for user in collection.find():
         users.append({"id": user["ID"], "name": user["Name"], "year": user["Year"], "salary": user["Salary"]})
     logger.info(f'show_user {users=}')
-    return render_template("users_list.html", users=users)
+    return render_template("users_list.html", users=users, prefix=prefix)
 
 
-@app.route("/add", methods=['GET', 'POST'])
+@app.route(f"{prefix}/add", methods=['GET', 'POST'])
 def add_user():
     """Добавление новой записи в базу данных"""
     if request.method == 'GET':
-        return render_template("user_details.html", user={})
+        return render_template("user_details.html", user={}, prefix=prefix)
     if request.method == "POST":
         id = int(request.form["id"])
         name = request.form["name"]
@@ -35,10 +36,10 @@ def add_user():
         new_user = {"ID": id, "Name": name, "Year": year, "Salary": salary}
         collection.insert_one(new_user)
         logger.info(f'POST {new_user=}')
-        return redirect('/app_1')
+        return redirect(f'{prefix}')
 
 
-@app.route("/update/<int:id>", methods=['GET', 'POST'])
+@app.route(f"{prefix}/update/<int:id>", methods=['GET', 'POST'])
 def update_user(id):
     """Обновление существующей записи в базе данных"""
     user_list = []
@@ -47,7 +48,7 @@ def update_user(id):
         for user in collection.find({'ID': id}):
             user_list.append({"id": user["ID"], "name": user["Name"], "year": user["Year"], "salary": user["Salary"]})
             logger.info(f'GET {user_list=}')
-            return render_template("user_details.html", user=user_list[0])
+            return render_template("user_details.html", user=user_list[0], prefix=prefix)
     if request.method == 'POST':
         name = request.form["name"]
         year = int(request.form["year"])
@@ -57,18 +58,18 @@ def update_user(id):
         update_value = {"$set": {"Name": name, "Year": year, "Salary": salary}}
         collection.update_one(condition, update_value)
         logger.info(f'POST {id=}, {update_value=}')
-        return redirect("/app_1")
+        return redirect(f'{prefix}')
 
 
-@app.route("/delete/<int:id>")
+@app.route(f"{prefix}/delete/<int:id>")
 def delete_user(id):
     """Удаление существующей записи в базе данных"""
     collection = _connect_db()
     collection.delete_one({"ID": id})
     logger.info(f'DELETE {id=}')
-    return redirect('/app_1')
+    return redirect(f'{prefix}')
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=80, debug=True)
-    # app.run(host="0.0.0.0", port=os.getenv('FLASK_PORT'), debug=True)
+    # app.run(host="0.0.0.0", port=80, debug=True)
+    app.run(host="0.0.0.0", port=os.getenv('FLASK_PORT'), debug=True)
--- src/mongo_db_create.py
+++ src/mongo_db_create.py
@@ -5,14 +5,18 @@ from settings import logger
 def fill_data() -> None:
     """Первоначальное заполнение базы данных примерами"""
     collection = _connect_db()
-    collection.insert_many([
-        {"ID": 1, "Name": "Misha", "Year": 1990, "Salary": 2000},
-        {"ID": 2, "Name": "Tolya", "Year": 1991, "Salary": 2200},
-        {"ID": 3, "Name": "Alex", "Year": 1992, "Salary": 1800},
-        {"ID": 4, "Name": "Mark", "Year": 1993, "Salary": 2500},
-        {"ID": 5, "Name": "Sasha", "Year": 1994, "Salary": 3000}
-    ])
-    logger.info('Первоначальное заполнение базы данных примерами')
+    count = collection.count_documents({})
+    if not count:
+        collection.insert_many([
+            {"ID": 1, "Name": "Misha", "Year": 1990, "Salary": 2000},
+            {"ID": 2, "Name": "Tolya", "Year": 1991, "Salary": 2200},
+            {"ID": 3, "Name": "Alex", "Year": 1992, "Salary": 1800},
+            {"ID": 4, "Name": "Mark", "Year": 1993, "Salary": 2500},
+            {"ID": 5, "Name": "Sasha", "Year": 1994, "Salary": 3000}
+        ])
+        logger.info(f'Первоначальное заполнение. Добавлено {collection.count_documents({})} документов')
+    else:
+        logger.info(f'База уже содержит {count} документов')
 
 
 if __name__ == '__main__':
